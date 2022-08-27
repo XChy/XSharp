85a46669c0f91f4bc4419ee1df839dd560a836fe
xxs_chy <xxs_chy@outlook.com>
2022-08-27 21:45:40 +0800

    Complete the information of operators

--- XSharp/Assoc.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-enum Assoc {
-	LeftToRight,
-	RightToLeft
-};
---
+++ XSharp/OperInfo.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "xsharp_global.h"
+#include "XString.h"
+//Assocativity
+enum Assoc {
+	LeftToRight,
+	RightToLeft
+};
+
+enum UnaryOperatorPlace {
+	BeforeValue=0b01,
+	AfterValue=0b10,
+	All= BeforeValue| AfterValue
+};
+
+struct BinaryOperatorInfo
+{
+	int priority;
+	Assoc assoc;
+};
+
+struct UnaryOperatorInfo
+{
+	int priority;
+	UnaryOperatorPlace place;
+	Assoc assoc;
+};
+
+namespace XSharp {
+	static std::unordered_map<XString, BinaryOperatorInfo> binaryOperatorPriority = {
+		{"+",{5,LeftToRight}},//{operator, {priority,associativity}}
+		{"-",{5,LeftToRight}},
+		{"*",{4,LeftToRight}},
+		{"/",{4,LeftToRight}},
+		{"^",{11,LeftToRight}},
+		{"&",{10,LeftToRight}},
+		{"%",{4,LeftToRight}},
+		{"!=",{9,LeftToRight}},
+		{"=",{16,RightToLeft}},
+		{"==",{9,LeftToRight}},
+		{"+=",{16,RightToLeft}},
+		{"-=",{16,RightToLeft}},
+		{"*=",{16,RightToLeft}},
+		{"/=",{16,RightToLeft}},
+		{"<<=",{16,RightToLeft}},
+		{">>=",{16,RightToLeft}},
+		{"%=",{16,RightToLeft}},
+		{"&=",{16,RightToLeft}},
+		{"|=",{16,RightToLeft}},
+		{"^=",{16,RightToLeft}},
+		{"=",{16,RightToLeft}},
+		{"<",{8,LeftToRight}},
+		{">",{8,LeftToRight}},
+		{"<=",{8,LeftToRight}},
+		{">=",{8,LeftToRight}},
+		{"?",{15,LeftToRight}},
+		{":",{15,LeftToRight}},
+	};
+
+	static std::unordered_map<XString, UnaryOperatorInfo> UnaryOperatorPriority = {
+		{"+",{3,BeforeValue,RightToLeft}},//{operator,{priority,place,associativity}}
+		{"-",{3,BeforeValue,RightToLeft}},
+		{"++",{3,All,RightToLeft}},
+		{"--",{3,All,RightToLeft}},
+		{"!",{3,BeforeValue,RightToLeft}}
+	};
+}
