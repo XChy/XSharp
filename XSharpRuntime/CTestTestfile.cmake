c51419af0ec10090c1ceece8ac88dd1ed27aca97
XChy <xxs_chy@outlook.com>
2023-03-06 00:53:49 +0800

    Apply GTest

---
+++ XSharpRuntime/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/xchy/Projects/XSharp/XSharpRuntime
+# Build directory: /home/xchy/Projects/XSharp/XSharpRuntime
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- test/test_utils.cpp
+++ test/test_utils.cpp
@@ -1,15 +1,39 @@
 #include "XSharp/XString.h"
-#include <cstdio>
-#include <iostream>
+#include "gtest/gtest.h"
 
-int main()
+TEST(XStringTest, FromCString)
 {
-    XString s = "12345";
-    for (int i = 0; i < 1000; ++i) {
-        char buffer[100];
-        sprintf(buffer, ",%d", i);
-        s.append("[" + XString(buffer) + "]");
+    std::vector<const char*> v = {
+        "\n\t\0a1233141",
+        "helloworld",
+        "\0\0\0",
+        "",
+        "123112",
+        "Nothing is better than programming",
+        "Test Test Test Test Test Test Test Test Test Test Test Test Test Test "
+        "Test Test Test Test"};
+    for (auto cstr : v) {
+        ASSERT_STREQ(XString(cstr).toStdString().data(), cstr);
     }
-    std::cout << s.toStdString() << std::endl;
-    return 0;
 }
+
+TEST(XStringTest, Append)
+{
+    std::vector<const char*> v = {"\n\t\0a1233141", "helloworld", "\0", "",
+                                  "123112"};
+    for (auto cstr : v) {
+        ASSERT_STREQ(XString(cstr).toStdString().data(), cstr);
+    }
+}
+
+// int main()
+//{
+// XString s = "12345";
+// for (int i = 0; i < 1000; ++i) {
+// char buffer[100];
+// sprintf(buffer, ",%d", i);
+// s.append("[" + XString(buffer) + "]");
+//}
+// std::cout << s.toStdString() << std::endl;
+// return 0;
+//}
